a0edf53ae284b05a9ac35a2ba917ff4718657e02
zz-fe <214464812@qq.com>
2018-09-26 17:32:43 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # daily-algorithm
-每日一道算法题,共同进步,防止老年痴呆
+每日一道算法题,共同进步,防止老年痴呆 
+
+
+# 算法题
+
+
+## 
+观察下面的规律，写一个函数accum
+```
+//accum(“abcd”);    // “A-Bb-Ccc-Dddd”
+// accum(“RqaEzty”); // “R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy”
+// accum(“cwAt”);    // “C-Ww-Aaa-Tttt”
+```
+
+
---
+++ accum.js
@@ -0,0 +1,17 @@
+//accum(“abcd”);    // “A-Bb-Ccc-Dddd”
+// accum(“RqaEzty”); // “R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy”
+// accum(“cwAt”);    // “C-Ww-Aaa-Tttt”
+const accum = string => {
+  if (typeof string !== 'string')  throw new Error('string is invalid string') 
+    return Array.from(string).map((i,index)=>{
+       return `${i.toUpperCase()}${i.toLowerCase().repeat(index)}`
+    }).join('-')
+}
+console.log(accum('abcd'))
+
+const _accum = string => {
+   return Array.from(string, (i,index) => {
+     return  i.toLocaleUpperCase() + new Array(index).fill(i.toLocaleLowerCase()).join('')
+   }).join('-')
+}
+console.log(_accum('abcd'))
